8e2c341b3f0429b46b8440794f49ee2dcb9d9dc3
Fabio Villavicencio <favm72@gmail.com>
2023-01-21 23:49:35 -0500

    add custom swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwiperComponent } from './components/swiper/swiper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
     BrowserModule,
